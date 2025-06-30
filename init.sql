a1249f09c1ca2d4788c2b91fcc404015067ed983
Vidhya Mariappan <mvidhya@CEIIND.COM>
2025-06-30 16:21:37 +0530

    update app

---
+++ init.sql
@@ -0,0 +1,24 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Employees] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Department] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Employees] PRIMARY KEY ([Id])
+);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250630085226_initial', N'9.0.6');
+
+COMMIT;
+GO
+
--- obj/Release/net8.0/CrudApp1.AssemblyInfo.cs
+++ obj/Release/net8.0/CrudApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2649a1f6101bce37cdda704dad2aa48ab36b329")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
